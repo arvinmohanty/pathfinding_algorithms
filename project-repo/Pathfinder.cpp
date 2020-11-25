30be7520a00d21a4fd7cd267901ac8054d30c350
Tino Caer <ccaer2@linux-a2.ews.illinois.edu>
2020-11-25 12:14:45 -0600

    Add functions to .cpp files

--- project-repo/Pathfinder.cpp
+++ project-repo/Pathfinder.cpp
@@ -1,1 +1,19 @@
 #include "Pathfinder.h"
+
+Pathfinder::Pathfinder() {
+  
+}
+
+Pathfinder::Pathfinder(UndirectedGraph* graph) {
+
+}
+
+template<typename T>
+int Pathfinder::shortestPath(Node<T>* toFind) {
+
+}
+
+template<typename T>
+int Pathfinder::landmarkPath(Node<T>* landmark, Node<T> toGoTo) {
+  
+}
--- project-repo/UndirectedGraph.cpp
+++ project-repo/UndirectedGraph.cpp
@@ -1,1 +1,17 @@
 #include "UndirectedGraph.h"
+
+UndirectedGraph::UndirectedGraph() {
+  
+}
+
+UndirectedGraph::UndirectedGraph(string filename) {
+
+}
+
+void UndirectedGraph::load(string filename) {
+
+}
+
+UndirectedGraph::~UndirectedGraph() {
+
+}
