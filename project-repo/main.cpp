754bc8d1d38150faf1ca2ad96b766cad5bb966fc
Arvin Mohanty <arvinkm2@linux-a2.ews.illinois.edu>
2020-12-04 12:13:41 -0600

    “METHODSLAYEDOUT”

--- project-repo/main.cpp
+++ project-repo/main.cpp
@@ -5,26 +5,25 @@
 
 int main() {
     
-    //UndirectedGraph a("roadNet-CA.txt");
     UndirectedGraph a("project-repo/tests/testdata1");
-    Pathfinder p(a);
-
-    vector<int> arv = p.dfs(1);
-    for (int b : arv) std::cout << b <<std::endl;
-    // std::cout << arv.size();
-    // vector<int> arvi = p.dfs(1);
-    // for (int i : arvi) std::cout << i;
-
-
-
+   Pathfinder p(a);
  
-
-    // std::vector<int> arv = p.connectedComponent();
-    // for (int i : arv) std::cout << i;
-
-
-    
-
+   vector<int> arv = p.dfs(1);
+   for (int b : arv) std::cout << b <<std::endl;
+   std::cout << std::endl;
+ 
+ 
+   vector<int> connected = p.connectedComponent();
+   for (int b : connected) std::cout << b <<std::endl;
+   std::cout << std::endl;
+ 
+   vector<int> shortestPath = p.shortestPath(1, 384);
+   for (int b : shortestPath) std::cout << b << std::endl;
+   std::cout << std::endl;
+ 
+   vector<int> landmark_path = p.landmarkPath(1, 384, 1);
+   for (int b : landmark_path) std::cout << b << std::endl;
+   std::cout << std::endl;
     
     
 
