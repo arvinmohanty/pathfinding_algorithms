0fd739edb641a13aad4a799a723e7200d1de7e6b
Tino Caer <ccaer2@linux-a2.ews.illinois.edu>
2020-12-06 17:16:34 -0600

    Push edge case

--- project-repo/tests/testSP.cpp
+++ project-repo/tests/testSP.cpp
@@ -21,3 +21,12 @@ TEST_CASE("Test reserve basic shortest path") {
 
   REQUIRE(expected == path);
 }
+
+TEST_CASE("Path shouldn't exist for node that doesn't exist") {
+  UndirectedGraph graph("project-repo/tests/testdata2.txt");
+  Pathfinder pathfinder(graph);
+
+  std::vector<int> path = pathfinder.shortestPath(-1, 44);
+
+  REQUIRE(path.size() == 0);
+}
