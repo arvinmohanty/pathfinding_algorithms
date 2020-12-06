e9cb33d3fdd7ba2d595b5fe899060d5efc14b107
Tino Caer <ccaer2@linux-a2.ews.illinois.edu>
2020-12-06 17:38:51 -0600

    Add more test cases

--- project-repo/tests/testSP.cpp
+++ project-repo/tests/testSP.cpp
@@ -10,6 +10,14 @@ TEST_CASE("Test basic shortest path") {
   std::vector<int> path = pathfinder.shortestPath(40, 44);
 
   REQUIRE(expected == path);
+
+  UndirectedGraph graph2("project-repo/tests/testdata1");
+  Pathfinder pathfinder2(graph2);
+
+  expected = {8, 7, 9, 10, 11, 12, 13, 94};
+  path = pathfinder2.shortestPath(8, 94);
+
+  REQUIRE(expected == path);
 }
 
 TEST_CASE("Test reserve basic shortest path") {
@@ -18,6 +26,14 @@ TEST_CASE("Test reserve basic shortest path") {
 
   std::vector<int> expected = {44, 43, 42, 41, 40};
   std::vector<int> path = pathfinder.shortestPath(44, 40);
+
+  REQUIRE(expected == path);
+
+  UndirectedGraph graph2("project-repo/tests/testdata1");
+  Pathfinder pathfinder2(graph2);
+
+  expected = {94, 13, 12, 11, 10, 9, 7, 8};
+  path = pathfinder2.shortestPath(94, 8);
 
   REQUIRE(expected == path);
 }
@@ -30,6 +46,9 @@ TEST_CASE("Path shouldn't exist for node whose index is out of bounds") {
   REQUIRE(path.size() == 0);
   path = pathfinder.shortestPath(40, 1000000);
   REQUIRE(path.size() == 0);
+
+  UndirectedGraph graph2("project-repo/tests/testdata1");
+  Pathfinder pathfinder2(graph);
 }
 
 TEST_CASE("Path shouldn't exist for nodes who aren't connected") {
@@ -37,6 +56,15 @@ TEST_CASE("Path shouldn't exist for nodes who aren't connected") {
   Pathfinder pathfinder(graph);
 
   std::vector<int> path = pathfinder.shortestPath(5000, 54);
+
+  REQUIRE(path.size() == 0);
+}
+
+TEST_CASE("Path to the same node should be empty") {
+  UndirectedGraph graph("project-repo/tests/testdata2.txt");
+  Pathfinder pathfinder(graph);
+
+  std::vector<int> path = pathfinder.shortestPath(54, 54);
 
   REQUIRE(path.size() == 0);
 }
