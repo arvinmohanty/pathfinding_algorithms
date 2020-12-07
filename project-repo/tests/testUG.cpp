86ccfbd76466473eeb315bc3c5f0cc1d10504ead
Tino Caer <ccaer2@linux-a2.ews.illinois.edu>
2020-12-06 18:00:21 -0600

    Add some test cases for UndirectedGraph

--- project-repo/tests/testUG.cpp
+++ project-repo/tests/testUG.cpp
@@ -6,4 +6,12 @@ TEST_CASE("UndirectedGraph can be loaded in") {
   UndirectedGraph graph("project-repo/tests/testdata2.txt");
 
   REQUIRE(graph.adj_list.size() == 1648645);
+  REQUIRE(graph.edge_count == 73);
+}
+
+TEST_CASE("UndirectedGraph add edge") {
+  UndirectedGraph graph("project-repo/tests/testdata2.txt");
+
+  graph.addEdge(5000, 2);
+  REQUIRE(graph.edge_count == 74);
 }
